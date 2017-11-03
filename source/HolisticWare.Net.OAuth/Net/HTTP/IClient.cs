acc5023ec20a623122956b17137ef1c93b42e78f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-03 12:54:56 +0100

    initial commit source and modified gitignore

---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/IClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using System.Net;
+
+namespace HolisticWare.Net.HTTP
+{
+    /// <summary>
+    /// HTTP Client interface
+    ///     Fluent interface
+    ///     mimic CURL
+    /// </summary>
+    public partial interface IClient
+    {
+        // Fluent API IClient.Fluent.API.cs
+
+        // Request API IClient.Requests.API.cs
+    }
+}
---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/OAuth/OAuth2/ResponseType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HolisticWare.Net.OAuth.OAuth2
+//namespace Xamarin.Auth.OAuth.OAuth2
+{
+    public partial class ResponseType
+    {
+        public ResponseType()
+        {
+            
+            return;
+        }
+    }
+}
