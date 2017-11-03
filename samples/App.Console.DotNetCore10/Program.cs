c417a5890f362057eda8df25cbe5d439822e2353
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-03 12:55:45 +0100

    sample for console

---
+++ samples/App.Console.DotNetCore10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using HolisticWare.Net.Http;
+
+namespace App.Console.DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c = new Client();
+
+            Console.WriteLine($"Demo");
+            return;
+        }
+    }
+}
